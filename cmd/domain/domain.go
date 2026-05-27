2d1872ec436925a4812093b35896e778ad5765ae
Michele Giornetta <michele@civo.com>
2026-05-27 18:16:46 +0200

    feat(domain): add domain record update command
    Implements `civo domain record update DOMAIN/DOMAIN_ID RECORD_ID` so
    records can be modified in place instead of remove-and-recreate. Only
    flags the user passes are changed; other fields keep their current
    values.
    
    Closes #386

--- cmd/domain/domain.go
+++ cmd/domain/domain.go
@@ -45,6 +45,7 @@ func init() {
 	domainRecordCmd.AddCommand(domainRecordCreateCmd)
 	domainRecordCmd.AddCommand(domainRecordShowCmd)
 	domainRecordCmd.AddCommand(domainRecordRemoveCmd)
+	domainRecordCmd.AddCommand(domainRecordUpdateCmd)
 
 	/*
 		Flags for domain record create cmd
@@ -54,5 +55,14 @@ func init() {
 	domainRecordCreateCmd.Flags().StringVarP(&recordValue, "value", "v", "", "the value of the record")
 	domainRecordCreateCmd.Flags().IntVarP(&recordTTL, "ttl", "t", 600, "The TTL of the record")
 	domainRecordCreateCmd.Flags().IntVarP(&recordPriority, "priority", "p", 0, "the priority of record only for SRV and MX record")
+
+	/*
+		Flags for domain record update cmd
+	*/
+	domainRecordUpdateCmd.Flags().StringVarP(&updateRecordName, "name", "n", "", "the name of the record")
+	domainRecordUpdateCmd.Flags().StringVarP(&updateRecordType, "type", "e", "", "type of the record (A, CNAME, TXT, SRV, MX, NS)")
+	domainRecordUpdateCmd.Flags().StringVarP(&updateRecordValue, "value", "v", "", "the value of the record")
+	domainRecordUpdateCmd.Flags().IntVarP(&updateRecordTTL, "ttl", "t", 0, "The TTL of the record")
+	domainRecordUpdateCmd.Flags().IntVarP(&updateRecordPriority, "priority", "p", 0, "the priority of record only for SRV and MX record")
 
 }
---
+++ cmd/domain/domain_record_update.go
@@ -0,0 +1,129 @@
+package domain
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/civo/civogo"
+	"github.com/civo/cli/common"
+	"github.com/civo/cli/config"
+	"github.com/civo/cli/utility"
+	"github.com/spf13/cobra"
+)
+
+var updateRecordName, updateRecordType, updateRecordValue string
+var updateRecordTTL, updateRecordPriority int
+
+var domainRecordUpdateCmd = &cobra.Command{
+	Use:     "update [DOMAIN|DOMAIN_ID] [RECORD_ID]",
+	Aliases: []string{"change", "modify"},
+	Short:   "Update a domain record",
+	Args:    cobra.MinimumNArgs(2),
+	Example: "civo domain record update DOMAIN/DOMAIN_ID RECORD_ID [flags]",
+	Run: func(cmd *cobra.Command, args []string) {
+		client, err := config.CivoAPIClient()
+		if err != nil {
+			utility.Error("Creating the connection to Civo's API failed with %s", err)
+			os.Exit(1)
+		}
+
+		domain, err := client.FindDNSDomain(args[0])
+		if err != nil {
+			if errors.Is(err, civogo.ZeroMatchesError) {
+				utility.Error("sorry there is no %s domain in your account", utility.Red(args[0]))
+				os.Exit(1)
+			}
+			if errors.Is(err, civogo.MultipleMatchesError) {
+				utility.Error("sorry we found more than one domain with that name in your account")
+				os.Exit(1)
+			}
+			utility.Error("Unable to find the domain for your search %s", err)
+			os.Exit(1)
+		}
+
+		record, err := client.GetDNSRecord(domain.ID, args[1])
+		if err != nil {
+			if errors.Is(err, civogo.ErrDNSRecordNotFound) {
+				utility.Error("sorry there is no %s domain record in your account", utility.Red(args[1]))
+				os.Exit(1)
+			}
+			utility.Error("%s", err)
+			os.Exit(1)
+		}
+
+		// Seed the config from the existing record so unspecified fields are preserved
+		// (the API call is a full-replace PUT).
+		recordConfig := &civogo.DNSRecordConfig{
+			Type:     record.Type,
+			Name:     record.Name,
+			Value:    record.Value,
+			TTL:      record.TTL,
+			Priority: record.Priority,
+		}
+
+		if cmd.Flags().Changed("name") {
+			recordConfig.Name = updateRecordName
+		}
+
+		if cmd.Flags().Changed("value") {
+			recordConfig.Value = updateRecordValue
+		}
+
+		if cmd.Flags().Changed("ttl") {
+			recordConfig.TTL = updateRecordTTL
+		}
+
+		if cmd.Flags().Changed("priority") {
+			recordConfig.Priority = updateRecordPriority
+		}
+
+		if cmd.Flags().Changed("type") {
+			// Sanitise the record type
+			updateRecordType = strings.ReplaceAll(updateRecordType, " ", "")
+
+			if updateRecordType == "A" || updateRecordType == "a" || updateRecordType == "alias" {
+				recordConfig.Type = civogo.DNSRecordTypeA
+			}
+
+			if updateRecordType == "CNAME" || updateRecordType == "cname" || updateRecordType == "canonical" {
+				recordConfig.Type = civogo.DNSRecordTypeCName
+			}
+
+			if updateRecordType == "MX" || updateRecordType == "mx" || updateRecordType == "mail" {
+				recordConfig.Type = civogo.DNSRecordTypeMX
+			}
+
+			if updateRecordType == "TXT" || updateRecordType == "txt" || updateRecordType == "text" {
+				recordConfig.Type = civogo.DNSRecordTypeTXT
+			}
+
+			if updateRecordType == "SRV" || updateRecordType == "srv" || updateRecordType == "service" {
+				recordConfig.Type = civogo.DNSRecordTypeSRV
+			}
+
+			if updateRecordType == "NS" || updateRecordType == "ns" || updateRecordType == "nameserver" {
+				recordConfig.Type = civogo.DNSRecordTypeNS
+			}
+		}
+
+		updatedRecord, err := client.UpdateDNSRecord(record, recordConfig)
+		if err != nil {
+			utility.Error("%s", err)
+			os.Exit(1)
+		}
+
+		ow := utility.NewOutputWriterWithMap(map[string]string{"id": updatedRecord.ID, "name": updatedRecord.Name})
+
+		switch common.OutputFormat {
+		case "json":
+			ow.WriteSingleObjectJSON(common.PrettySet)
+		case "custom":
+			ow.WriteCustomOutput(common.OutputFields)
+		default:
+			fmt.Printf("Updated %s record %s for %s with a TTL of %s seconds and with a priority of %s with ID %s\n", utility.Green(string(updatedRecord.Type)), utility.Green(updatedRecord.Name), utility.Green(domain.Name), utility.Green(strconv.Itoa(updatedRecord.TTL)), utility.Green(strconv.Itoa(updatedRecord.Priority)), utility.Green(updatedRecord.ID))
+		}
+	},
+}
